e6bd67f2609224b1125d13bba78e18b2f2978070
Boris Grozev <boris@jitsi.org>
2024-04-23 13:50:23 -0500

    Add JitsiXmppStringprep.

---
+++ src/main/kotlin/org/jitsi/xmpp/Smack.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright @ 2024 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.xmpp
+
+import org.jitsi.utils.logging2.createLogger
+import org.jitsi.xmpp.stringprep.JitsiXmppStringprep
+import org.jivesoftware.smack.SmackConfiguration
+import org.jivesoftware.smack.parsing.ExceptionLoggingCallback
+import org.jivesoftware.smackx.bytestreams.socks5.Socks5Proxy
+import org.jxmpp.JxmppContext
+import org.jxmpp.jid.impl.JidCreate
+
+object Smack {
+    val logger = createLogger()
+
+    fun initialize() {
+        logger.info("Setting XML parsing limits.")
+        System.setProperty("jdk.xml.entityExpansionLimit", "0")
+        System.setProperty("jdk.xml.maxOccurLimit", "0")
+        System.setProperty("jdk.xml.elementAttributeLimit", "524288")
+        System.setProperty("jdk.xml.totalEntitySizeLimit", "0")
+        System.setProperty("jdk.xml.maxXMLNameLimit", "524288")
+        System.setProperty("jdk.xml.entityReplacementLimit", "0")
+
+        // Force XmppStringPrepUtil to load before we override the context, otherwise it gets reverted.
+        // https://github.com/igniterealtime/jxmpp/pull/44
+        JidCreate.from("example.com")
+        logger.info("Using JitsiXmppStringprep.")
+        JxmppContext.setDefaultXmppStringprep(JitsiXmppStringprep.INSTANCE)
+
+        // if there is a parsing error, do not break the connection to the server(the default behaviour) as we need
+        // it for the other conferences.
+        SmackConfiguration.setDefaultParsingExceptionCallback(ExceptionLoggingCallback())
+        Socks5Proxy.setLocalSocks5ProxyEnabled(false)
+    }
+}
---
+++ src/main/kotlin/org/jitsi/xmpp/stringprep/JitsiXmppStringprep.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright @ 2024 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.xmpp.stringprep
+
+import org.jxmpp.stringprep.XmppStringprep
+import org.jxmpp.stringprep.XmppStringprepException
+import org.jxmpp.stringprep.rocksxmppprecis.RocksXmppPrecisStringprep
+import rocks.xmpp.precis.PrecisProfile
+import java.net.IDN
+import java.text.Normalizer
+import java.util.regex.Pattern
+
+/**
+ * Extends [RocksXmppPrecisStringprep] to allow underscores (_) in the domain part.
+ *
+ * This is needed because jitsi-meet URLs of the form https://domain/tenant/room get translated into a JID of the
+ * form room@tenant.conference.domain, and the tenant field has been allowed to use underscores for a long time (in
+ * fact '.' in the tenant is translated into '_').
+ */
+class JitsiXmppStringprep : XmppStringprep by RocksXmppPrecisStringprep.INSTANCE {
+    override fun domainprep(string: String?): String {
+        try {
+            return idnWithUnderscoreProfile.enforce(string)
+        } catch (e: IllegalArgumentException) {
+            throw XmppStringprepException(string, e)
+        }
+    }
+
+    companion object {
+        val INSTANCE = JitsiXmppStringprep()
+        private val idnWithUnderscoreProfile = IDNWithUnderscoreProfile()
+    }
+}
+
+/**
+ * Based on [PrecisProfiles.IDN], but allows underscores.
+ */
+class IDNWithUnderscoreProfile : PrecisProfile(false) {
+    override fun prepare(input: CharSequence): String {
+        val str = input.toString()
+        val strNoUnderscore = str.trim('_').trimEnd('_').replace('_', '-')
+
+        // Throws if [strNoUnderscore] contains invalid characters
+        IDN.toASCII(strNoUnderscore, IDN.USE_STD3_ASCII_RULES)
+
+        return IDN.toUnicode(IDN.toASCII(str), IDN.USE_STD3_ASCII_RULES)
+    }
+
+    override fun applyWidthMappingRule(charSequence: CharSequence) = widthMap(charSequence)
+    override fun applyAdditionalMappingRule(charSequence: CharSequence) =
+        LABEL_SEPARATOR.matcher(charSequence).replaceAll(".")
+    override fun applyCaseMappingRule(charSequence: CharSequence) = charSequence.toString().lowercase()
+
+    override fun applyNormalizationRule(charSequence: CharSequence) =
+        Normalizer.normalize(charSequence, Normalizer.Form.NFC)
+
+    override fun applyDirectionalityRule(charSequence: CharSequence) = charSequence
+
+    companion object {
+        private const val DOTS: String = "[.\u3002\uFF0E\uFF61]"
+        private val LABEL_SEPARATOR: Pattern = Pattern.compile(DOTS)
+    }
+}
